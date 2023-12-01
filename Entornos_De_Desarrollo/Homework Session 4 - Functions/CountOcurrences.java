33b57efccf70c58c941f128e984bfbd087c689c7
roobcrack <39882967+roobcrack@users.noreply.github.com>
2023-12-01 15:13:46 +0100

    Add

---
+++ Entornos_De_Desarrollo/Homework Session 4 - Functions/CountOcurrences.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class CountOcurrences{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Phrase a: ");
+		String a = sc.nextLine();
+		System.out.print("Phrase b: ");
+		String b = sc.nextLine();
+		System.out.print("Insert: ");
+		int c = sc.nextInt();
+		System.out.print(countString(a, b, c););
+	}
+
+	public static bool countString(String a, String b, int c){
+
+	}
+}
