e6e94196b43750e3033c97cae6fdc4027d6ef23f
roobcrack <39882967+roobcrack@users.noreply.github.com>
2023-12-21 14:45:27 +0100

    Add

---
+++ Entornos_De_Desarrollo/Optional Exercises/Ex1.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Ex1{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int number, result;
+
+		//Ask for number
+		System.out.print("Insert one number: ");
+		number = sc.nextInt();
+		
+		System.out.printf("The first %d squared numbers are: ",number);
+		
+		//Print the values
+		for(int i=1;i<=number;i++){
+			System.out.print(squareCalc(number, i));
+			
+			if(i<number)
+				System.out.print(", ");
+		}
+	}
+
+
+	public static int squareCalc(int number, int i){
+		//Calculate the values
+		return i * i;
+	}
+}
