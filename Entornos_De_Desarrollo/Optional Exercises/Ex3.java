15c34c34b5839b20a9f383f1342a77df4e149daf
roobcrack <39882967+roobcrack@users.noreply.github.com>
2023-12-22 21:57:39 +0100

    Add

--- Entornos_De_Desarrollo/Optional Exercises/Ex3.java
+++ Entornos_De_Desarrollo/Optional Exercises/Ex3.java
@@ -1,11 +1,19 @@
 import java.util.Scanner;
-public class Main{
+
+public class Ex3{
 	public static void main(String[] args){
-		int number;
-		
-	}
-	
-	public static void primeCalc(){
-		for(int i<=;
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Insert number: ");
+		int number = sc.nextInt();
+
+		System.out.printf("Prime numbers until %d are:", number);
+		for(int i=2;i<=number;i++){
+			for (int j=2;j<=Math.sqrt(i);j++) {
+	            if (i % j != 0) {
+	                System.out.printf(" %d",j);
+	            }
+	        }
+	    }
 	}
 }
