e7e9d237763ecd52ee95333c0b7584bad58821ff
roobcrack <39882967+roobcrack@users.noreply.github.com>
2024-01-17 20:33:42 +0100

    Add

--- Entornos_De_Desarrollo/Compulsory exercises/testing.java
+++ Entornos_De_Desarrollo/Compulsory exercises/testing.java
@@ -1,9 +1,10 @@
 public class testing{
 	public static void main(String[] args){
-		System.out.println("   _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ ");
+		//45
+		System.out.println("   _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _  ");
 		System.out.println(" /                                           \\");
 		System.out.println("|                                             |");
-		System.out.println("|              Choose your coffee             |");
+		System.out.printf("|%s%s%s%s|\n", spaces, text, spaces, odd);
 		System.out.println("|                                             |");
 		System.out.println("|                                             |");
 		System.out.println("|         1.Coffee                            |");
@@ -12,6 +13,21 @@ public class testing{
 		System.out.println("|                                             |");
 		System.out.println("|                  Insert:                    |");
 		System.out.println(" \\ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ /");
+		System.out.println("adadasdasdasdadasd");
+	menu();
+	}
+
+	public static void menu(){
+		String odd = "";
+		String text = "Choose your coffeeeeeeeeeeeeeeeeeeee";
+		if(text.length()%2==0)
+			odd =" ";
+		int numberSpaces = (45-text.length())/2;
+		String spaces = " ".repeat(numberSpaces);
+		//---
+		System.out.printf("  %s\n", " _".repeat(21));
+		System.out.printf(" /%s \\\n", "  ".repeat(21));
+		System.out.printf("|%s%s%s%s|\n",spaces,text,spaces,odd);
 	}
 }
 
