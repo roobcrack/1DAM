c490b924156c7112877b53549d30330d6c418911
roobcrack <39882967+roobcrack@users.noreply.github.com>
2024-01-30 22:26:21 +0100

    Add

--- Bases_de_Datos/TEMA 4/Rubén_Martínez_TENDA_PC_DAM.sql
+++ Bases_de_Datos/TEMA 4/Rubén_Martínez_TENDA_PC_DAM.sql
@@ -48,7 +48,7 @@ CREATE TABLE inventario (
 CREATE TABLE preciosum (
     numpieza VARCHAR2(20) NOT NULL,
     numvend NUMBER NOT NULL,
-    id VARCHAR2(20),
+    idpreciosum VARCHAR2(20),
     preciounit NUMBER,
     diassum NUMBER,
     descuento NUMBER,
@@ -57,28 +57,28 @@ CREATE TABLE preciosum (
 
 
 ALTER TABLE preciosum
-ADD CONSTRAINT fk_numpieza
+ADD CONSTRAINT fk_preciosum_numpieza
     FOREIGN KEY (numpieza)
-    REFERENCES pieza (numpieza);
-ADD CONSTRAINT fk_numvend
+    REFERENCES pieza (numpieza)
+ADD CONSTRAINT fk_preciosum_numvend
     FOREIGN KEY (numvend)
     REFERENCES vendedor (numvend);
 
 ALTER TABLE pedido
-ADD CONSTRAINT fk_numvend
+ADD CONSTRAINT fk_pedido_numvend
     FOREIGN KEY (numvend)
     REFERENCES vendedor (numvend);
 
 ALTER TABLE linped
-ADD CONSTRAINT fk_numpieza
+ADD CONSTRAINT fk_linped_numpieza
     FOREIGN KEY (numpieza)
     REFERENCES pieza (numpieza)
-ADD CONSTRAINT fk_numpedido
+ADD CONSTRAINT fk_linped_numpedido
     FOREIGN KEY (numpedido)
     REFERENCES pedido (numpedido);
 
 ALTER TABLE linped
-ADD CONSTRAING uk_numpieza
+ADD CONSTRAINT uk_linped_numpieza
     UNIQUE (numpieza);
 
 
@@ -92,7 +92,7 @@ INSERT INTO pieza (numpieza, nompieza, preciovent, descripcion)
 VALUES ('A-1001-L', 'MOUSE ADL 3B', 7, 'Periferico de entrada');
 
 INSERT INTO pieza (numpieza, preciovent)
-VALUES ('C-1002-H', 4,);
+VALUES ('C-1002-H', 4);
 
 INSERT INTO pieza (numpieza, preciovent)
 VALUES ('C-1002-J', 0);
@@ -129,7 +129,7 @@ INSERT INTO vendedor (numvend, nomvend, nombrecomer, calle, ciudad, provincia)
 VALUES (5, 'Juanito Reina Princesa', 'LA''DECAI', 'Asis, 10', 'Gijon', 'Asturias');
 
 INSERT INTO vendedor (numvend, nomvend, nombrecomer, calle, ciudad, provincia)
-VALUES (6, 'Manuel Perez Rodriguez', 'SOFTHARD', '2223344567', 'Temul, 1', 'Toledo', 'Toledo');
+VALUES (6, 'Manuel Perez Rodriguez', 'SOFTHARD', '2223344567', 'Temul, 1', 'Toledo');
 
 INSERT INTO vendedor (numvend, nomvend, nombrecomer)
 VALUES (100, 'Toñi Garcia', 'SOFTHARD'); 
@@ -156,22 +156,103 @@ CREATE SEQUENCE seq_preciosum
 START WITH 1
 INCREMENT BY 1;
 
-INSERT INTO preciosum (id, numvend, numpieza, preciounit, diassum, descuento)
+INSERT INTO preciosum (idpreciosum, numvend, numpieza, preciounit, diassum, descuento)
 VALUES (seq_preciosum.nextval, 3, 'A-1001-L', 5, 3, 10);
 
-INSERT INTO preciosum (id, numvend, numpieza, preciounit, diassum)
+INSERT INTO preciosum (idpreciosum, numvend, numpieza, preciounit, diassum)
 VALUES (seq_preciosum.nextval, 200, 'C-1002-H', 2, 5);
 
-INSERT INTO preciosum (id, numvend, numpieza, diassum)
+INSERT INTO preciosum (idpreciosum, numvend, numpieza, diassum)
 VALUES (seq_preciosum.nextval, 5, 'DK 144-0002-P', 2);
 
-INSERT INTO preciosum (id, numvend, numpieza, diassum, descuento)
+INSERT INTO preciosum (idpreciosum, numvend, numpieza, diassum, descuento)
 VALUES (seq_preciosum.nextval, 100, 'O-0001-PP', 2, 5);
 
 /*G)*/
 INSERT INTO pieza (numpieza, preciovent,)
 VALUES ('M-0002-JT', 100000000000,50);
+/*
+No, no lo permite e imprime este error: 
 
+Error starting at line : 86 in command -
+INSERT INTO pieza (numpieza, preciovent,)
+VALUES ('M-0002-JT', 100000000000,50)
+Error at Command Line : 86 Column : 41
+Error report -
+SQL Error: ORA-01747: invalid user.table.column, table.column, or column specification
+01747. 00000 -  "invalid user.table.column, table.column, or column specification"
+*Cause:    
+*Action:*/
 
 /*ACTIVIDAD 2*/
 /*A)*/
+UPDATE pedido
+SET fecha = TO_DATE('01/01/2024', 'DD/MM/YYYY');
+
+/*B)*/
+UPDATE vendedor
+SET telefono = 'SIN TELEFONO' WHERE telefono IS NULL;
+
+/*C)*/
+UPDATE vendedor
+SET (ciudad, provincia) = (
+    SELECT provincia, ciudad FROM vendedor WHERE numvend = 6
+) WHERE provincia <> 'Alicante';
+
+/*D)*/
+UPDATE preciosum
+SET preciounit = 10 WHERE preciounit IS NULL;
+
+/*E)*/
+UPDATE pieza
+SET preciovent = preciovent + 100 WHERE nompieza IN ('USB', 'DISCO INTERNO%', 'DISCO EXTERNO%');
+
+/*F)*/
+SAVEPOINT savepoint1;
+
+UPDATE pieza
+SET preciovent = 3 WHERE numpieza IN ('A-1001-L', 'C-1002-H', 'C-1002-J', 'C-400-Z', 'DK 144-0001', 'DK 144-0002-P', 'O-0001-PP');
+
+ROLLBACK TO SAVEPOINT savepoint1;
+
+
+
+
+
+/*ACTIVIDAD 3*/
+
+/*A)*/
+DELETE FROM pieza WHERE descripcion IS NULL;
+
+/*B)*/
+DELETE FROM pieza WHERE numvend IN (
+    SELECT numvend FROM vendedor WHERE nombre = 'JUANITOO REINA PRINCESA'
+);
+
+/*C)*/
+TRUNCATE TABLE pedido;
+
+/*D)*/
+DELETE FROM pieza WHERE numpieza IN ('C-1002-H', 'A-1001-L');
+
+
+
+
+
+
+/*ACTIVIDAD 4*/
+/*ON DELETE CASCADE: si eliminas una fila de la tabla principal, se elimina en el resto de tablas asociadas
+
+ON DELETE SET NULL: si eliminas una fila de la tabla principal, aparecerá como null en el resto de tablas asociadas 
+*/
+
+
+
+
+/*ACTIVIDAD 5*/
+/*
+COMMIT: confirma y guarda permanentemente los cambios realizados
+ROLLBACK: deshace todos los cambios y vuelve al punto de guardado
+SAVEPOINT: establece un punto de guardado
+SET TRANSACTION: establece caracteristicas de una transaccion como diversas propiedades de control
+*/
