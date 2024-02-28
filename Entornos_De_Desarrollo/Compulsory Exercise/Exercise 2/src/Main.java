75c59e1aa0ba37d20d7198c13205d8465bc4eb63
roobcrack <39882967+roobcrack@users.noreply.github.com>
2024-02-28 17:24:43 +0100

    Add

--- Entornos_De_Desarrollo/Compulsory Exercise/Exercise 2/src/Main.java
+++ Entornos_De_Desarrollo/Compulsory Exercise/Exercise 2/src/Main.java
@@ -1,106 +1,99 @@
 import java.util.Scanner;
 
-
-public class Exercise2{
-    public static void main(String[] args){
+public class Main{
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String[] ingredientsNames = {"water", "coffee", "cup", "decaffeinated coffee", "powdered milk", "powdered cream" };
-        int[] ingredientsAmmount = { 5, 4, 4, 3, 2, 1};
+        String[] ingredientsNames = {"water", "coffee", "cup", "decaffeinated coffee", "powdered milk", "powdered cream"};
+        int[] ingredientsAmmount = {5, 4, 4, 3, 2, 1};
         int[] ingredientsUsed = new int[3];
-        int insert=-1, sugarAmount, money;
+        int insert=-1, sugarStock=3, sugarAmount=-1, money=-1;
 
-        do{
-            boolean done = false;
-            while(!done){
+        do {
+            try {
                 System.out.println("1.Coffee");
                 System.out.println("2.Decaffeinated");
                 System.out.print(" Insert: ");
-                try{
-                    insert = Integer.parseInt(sc.nextLine());
-                } catch(Exception e){
-                    System.out.println("Error text parsing: "+e.getMessage());
-                }
-                if(insert>=0 && insert <=2)
-                    done=true;
-                else
-                    System.out.println("Error number range: number must be from 0 to 2");
-            }
-            switch(insert){
-                case 1:
-                    ingredientsUsed = new int[] {0, 1, 2};
-                    break;
-                case 2:
-                    ingredientsUsed = new int[] {0, 3, 2};
-                    break;
-            }
 
-            if(insert==0)
-                System.out.println("Closing program...");
-            else if(checkIfIngredients(ingredientsNames, ingredientsAmmount, ingredientsUsed)!=null)
-                System.out.println("EMPTY: "+checkIfIngredients(ingredientsNames, ingredientsAmmount, ingredientsUsed));
-            else{
-                boolean exit=false;
-                do{
-                    try{
+                insert = Integer.parseInt(sc.nextLine());
+                if (insert < 0 || insert > 2)
+                    throw new Exception("insert a valid number");
+            } catch (Exception e) {
+                System.out.println("-Error: " + e.getMessage());
+            }
+        } while (insert < 0 || insert > 2);
+        if (insert == 0)
+            System.out.println("Closing program...");
+        else {
+             if (checkIfIngredients(ingredientsNames, ingredientsAmmount, ingredientsUsed) != null)
+                System.out.println("EMPTY: " + checkIfIngredients(ingredientsNames, ingredientsAmmount, ingredientsUsed));
+            else if (sugarStock > 0) {
+                do {
+                    try {
                         System.out.println("Insert the amount of sugar from 0-5");
                         System.out.print("  Insert: ");
                         sugarAmount = Integer.parseInt(sc.nextLine());
-                        if(sugarAmount<=5 && sugarAmount >=0)
-                            exit = true;
-                        else
-                            System.out.println("Error: amount must be from 0-5");
-
-                    } catch(Exception e){
-                        System.out.println("Error: "+e.getMessage());
+                        if (sugarAmount > sugarStock) {
+                            throw new Exception("ammount must be from 0-5");
+                        }
+                    } catch (Exception e2) {
+                        System.out.println("-Error: " + e2.getMessage());
                     }
-                } while(!exit);
-                exit = false;
+                } while (sugarAmount > 5 || sugarAmount < 0);
+            } else
+                System.out.println("There is no sugar in stock");
 
-                do{
-                    try{
-                        System.out.println("Insert money");
-                        System.out.print("Insert: ");
-                        money = Integer.parseInt(sc.nextLine());
-                    } catch(Exception e){
-                        System.out.println("Error: "+e.getMessage());
-                    }
+            switch (insert) {
+                case 1:                                    //Coffee
+                    ingredientsUsed = new int[]{0, 1, 2};
+                    break;
+                case 2:                                    //Decaffeinated
+                    ingredientsUsed = new int[]{0, 3, 2};
+                    break;
+            }
 
-                    if(money > 20)
-                        System.out.println("Error: max insert must be 20");
-                    else if(money==1)
+            do {
+                try {
+                    System.out.println("Insert money");
+                    System.out.print(" Insert: ");
+                    money = Integer.parseInt(sc.nextLine());
+
+                    if (money > 20 || money < 1)
+                        throw new Exception("insert must be max: 20 & min: 1");
+                    else if (money == 1)
                         System.out.println("There is no change");
-                    exit = true;
-                    if(money>1)
-                        System.out.println("Change is: "+returnChange(money-1));
-                    exit = true;
-                } while(!exit);
-                preparateCoffee(ingredientsAmmount, ingredientsUsed);
-            } while(insert!=0);
-        }
-
-        public static String checkIfIngredients(String[] ingredientsNames, int[] ingredientsAmmount, int[] ingredientsUsed){
-            for(int i=0; i<ingredientsUsed.length; i++){
-                if(ingredientsAmmount[ingredientsUsed[i]]<=0)
-                    return ingredientsNames[ingredientsUsed[i]];
-            }
-            return null;
-        }
-
-        public static String returnChange(int money){
-            String finalString="";
-            int[] billsAndCoins = {20, 10, 5, 2, 1};
-
-            for (int ammount : billsAndCoins) {
-                while (money >= ammount) {
-                    money -= ammount;
-                    finalString += ammount + " ";
+                    else
+                        System.out.println("Change is: " + returnChange(money - 1));
+                } catch (Exception e) {
+                    System.out.println("-Error: " + e.getMessage());
                 }
-            }
-            return finalString;
-        }
-
-        public static void preparateCoffee(Scanner sc, int[] ingredientsAmmount, int[] ingredientsUsed){
-            for(int i=0; i<ingredientsUsed.length; i++)
-                ingredientsAmmount[ingredientsUsed[i]]--;
+            } while (money <= 20 && money >= 1);
+            preparateCoffee(ingredientsAmmount, ingredientsUsed);
         }
     }
+
+    public static String checkIfIngredients(String[] ingredientsNames, int[] ingredientsAmmount, int[] ingredientsUsed){
+        for(int i=0; i<ingredientsUsed.length; i++){
+            if(ingredientsAmmount[ingredientsUsed[i]]<=0)
+                return ingredientsNames[ingredientsUsed[i]];
+        }
+        return null;
+    }
+
+    public static String returnChange(int money){
+        String finalString="";
+        int[] billsAndCoins = {20, 10, 5, 2, 1};
+
+        for (int ammount : billsAndCoins) {
+            while (money >= ammount) {
+                money -= ammount;
+                finalString += ammount + " ";
+            }
+        }
+        return finalString;
+    }
+
+    public static void preparateCoffee(int[] ingredientsAmmount, int[] ingredientsUsed){
+        for(int i=0; i<ingredientsUsed.length; i++)
+            ingredientsAmmount[ingredientsUsed[i]]--;
+    }
+}
--- Entornos_De_Desarrollo/Compulsory exercises/Exercise 2/Exercise2.java
+++ Entornos_De_Desarrollo/Compulsory exercises/Exercise 2/Exercise2.java
@@ -1,5 +1,4 @@
-+import java.util.Scanner;
-
+import java.util.Scanner;
 
 public class Exercise2{
 	public static void main(String[] args){
@@ -7,29 +6,27 @@ public class Exercise2{
 		String[] ingredientsNames = {"water", "coffee", "cup", "decaffeinated coffee", "powdered milk", "powdered cream" };
 		int[] ingredientsAmmount = { 5, 4, 4, 3, 2, 1};
 		int[] ingredientsUsed = new int[3];
-		int insert=-1, sugarAmount, money;
+		int insert, sugarAmount, money;
 
 		do{
-			boolean done = false;
-			while(!done){
-				System.out.println("1.Coffee");
-				System.out.println("2.Decaffeinated");
-				System.out.print(" Insert: ");
+			do{
 				try{
+					System.out.println("1.Coffee");
+					System.out.println("2.Decaffeinated");
+					System.out.print(" Insert: ");
+					
 					insert = Integer.parseInt(sc.nextLine());
+					if(insert<1 || insert > 2)
+						throw new Exception("insert a valid number");
 				} catch(Exception e){
 					System.out.println("Error text parsing: "+e.getMessage());
 				}
-				if(insert>=0 && insert <=2)
-					done=true;
-                else
-					System.out.println("Error number range: number must be from 0 to 2");
-			}
+			} while (insert < 1 || insert > 2);
 			switch(insert){
-				case 1: 
+				case 1: 									//Coffee
 					ingredientsUsed = new int[] {0, 1, 2};
 					break;
-				case 2: 
+				case 2: 									//Decaffeinated
 					ingredientsUsed = new int[] {0, 3, 2};
 					break;
 			}
@@ -39,41 +36,34 @@ public class Exercise2{
 			else if(checkIfIngredients(ingredientsNames, ingredientsAmmount, ingredientsUsed)!=null)
 				System.out.println("EMPTY: "+checkIfIngredients(ingredientsNames, ingredientsAmmount, ingredientsUsed));
 			else{
-                boolean exit=false;
                 do{
                     try{
                         System.out.println("Insert the amount of sugar from 0-5");
                         System.out.print("  Insert: ");
                         sugarAmount = Integer.parseInt(sc.nextLine());
-                        if(sugarAmount<=5 && sugarAmount >=0)
-                            exit = true;
-                        else
-                            System.out.println("Error: amount must be from 0-5");
-                        
+                        if(sugarAmount>5 || sugarAmount <0)
+                            throw new Exception("ammount must be from 0-5");
                     } catch(Exception e){
                         System.out.println("Error: "+e.getMessage());
                     }
-                } while(!exit);
-				exit = false;
+                } while(sugarAmount>5 || sugarAmount <0);
 
                 do{
                     try{
                         System.out.println("Insert money");
-                        System.out.print("Insert: ");
+                        System.out.print(" Insert: ");
                         money = Integer.parseInt(sc.nextLine()); 
-                    } catch(Exception e){
+
+	                    if(money > 20 || money < 1)
+	                        throw new Exception("insert must be max: 20 & min: 1");
+	                    else if(money==1)
+	                        System.out.println("There is no change");
+	                    else
+	                        System.out.println("Change is: "+returnChange(money-1));
+	                } catch(Exception e){
                         System.out.println("Error: "+e.getMessage());
                     }
-
-                    if(money > 20)
-                        System.out.println("Error: max insert must be 20");
-                    else if(money==1)
-                        System.out.println("There is no change");
-                    exit = true;
-                    if(money>1)
-                        System.out.println("Change is: "+returnChange(money-1));
-                    exit = true;
-                } while(!exit);
+                } while(money<=20 && money>=1);
                 preparateCoffee(ingredientsAmmount, ingredientsUsed);
 		} while(insert!=0);
 	}
