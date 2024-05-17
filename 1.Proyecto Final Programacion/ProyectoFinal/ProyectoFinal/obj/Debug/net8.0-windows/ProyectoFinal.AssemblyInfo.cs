3d5122224117d1796258c3f5085ed1795989fb4b
roobcrack <39882967+roobcrack@users.noreply.github.com>
2024-05-17 22:24:51 +0200

    Add

--- 1.Proyecto Final Programacion/ProyectoFinal/ProyectoFinal/obj/Debug/net8.0-windows/ProyectoFinal.AssemblyInfo.cs
+++ 1.Proyecto Final Programacion/ProyectoFinal/ProyectoFinal/obj/Debug/net8.0-windows/ProyectoFinal.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoFinal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1edba6b87cad783c199eec6a23746601c2b54e22")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoFinal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoFinal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
